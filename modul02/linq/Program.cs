6a7535fc163b8d91058269636de600f94e38c807
Kristian Dorland <kristian@dorland.dk>
2023-02-08 12:50:13 +0100

    Lidt eksempler og opgaveløsninger

---
+++ modul02/linq/Program.cs
@@ -0,0 +1,17 @@
+﻿Person[] people = new Person[]
+{
+    new Person { Name = "Jens Hansen", Age = 45, Phone = "+4512345678" },
+    new Person { Name = "Jane Olsen", Age = 22, Phone = "+4543215687" },
+    new Person { Name = "Tor Iversen", Age = 35, Phone = "+4587654322" },
+    new Person { Name = "Sigurd Nielsen", Age = 31, Phone = "+4512345673" },
+    new Person { Name = "Viggo Nielsen", Age = 28, Phone = "+4543217846" },
+    new Person { Name = "Rosa Jensen", Age = 23, Phone = "+4543217846" },
+};
+
+Console.WriteLine(string.Join(",  ", people.Select(p => p.Name + " " + p.Age)));
+
+class Person {
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Phone { get; set; }
+}
---
+++ modul02/rekursion/Program.cs
@@ -0,0 +1,15 @@
+﻿// Program.cs
+Console.WriteLine(Opgaver.Reverse("B"));
+Console.WriteLine(Opgaver.Reverse("Banankage"));
+
+class Opgaver {
+    public static string Reverse(string s) {
+        if (s.Length <= 1) {
+            return s;
+        } else {
+            return Reverse(s.Substring(1)) + s[0];
+        }
+    }
+}
+
+
