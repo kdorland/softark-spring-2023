99fd88adfb50fd16a813af871c0b624f984f40f6
Kristian Dorland <kristian@dorland.dk>
2023-02-15 08:48:48 +0100

    Løsning

---
+++ modul03/linq-opg3/Program.cs
@@ -0,0 +1,12 @@
+﻿var CreateWordFilterFn = (string[] filterWords) =>
+{
+    return (string input) =>
+    {
+        return string.Join(" ", input.Split(" ").Except(filterWords));
+    };
+};
+
+var badWords = new string[] { "shit", "fuck", "idiot" };
+var FilterBadWords = CreateWordFilterFn(badWords);
+Console.WriteLine(FilterBadWords("Sikke en gang shit"));
+// Udskriver: "Sikke en gang"
