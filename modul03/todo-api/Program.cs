5342a6514de9efdc27ccc5df1485d81fe0d36f0b
Kristian Dorland <kristian@dorland.dk>
2023-02-22 09:42:40 +0100

    Eksempel på Post

--- modul03/todo-api/Program.cs
+++ modul03/todo-api/Program.cs
@@ -1,13 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-Task[] todos = new Task[] {
-    new Task(0, "Husk at handle", false),
-    new Task(1, "Husk at betale skat", false),
-    new Task(2, "Husk at gøre rent", false),
-};
+int nextId = 0;
+List<Task> todos = new List<Task>();
+todos.Add(new Task(nextId++, "Husk at handle", false));
+todos.Add(new Task(nextId++, "Husk at betale skat", false));
+todos.Add(new Task(nextId++, "Husk at gøre rent", false));
 
-app.MapGet("/api/tasks", () => "???");
+app.MapGet("/api/tasks", () => todos.ToArray());
+
+app.MapPost("/api/tasks", (Task task) =>
+{
+    Task newTask = new Task(nextId++, task.text, task.done);
+    todos.Add(newTask);
+    return newTask;
+});
 
 app.Run();
 
